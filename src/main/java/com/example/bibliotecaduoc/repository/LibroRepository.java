56343b16e2564852ed14d864982bdc120e134481
to.guzmang@duocuc.cl <to.guzmang@duocuc.cl>
2026-03-26 21:19:13 -0300

    commit añadir libros

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -12,6 +12,20 @@ public class LibroRepository {
     //arreglo para guardar todos los libros
     private List<libro> listaLibros= new ArrayList<>();
 
+    public LibroRepository(){
+        listaLibros.add(new libro(1, "9789569646638", "Fuego y Sangre", "Penguin Random House Grupo Editorial", 2018, "George R. R. Martin"));
+        listaLibros.add(new libro(2, "9789563494150", "Quique Hache: El Mall Embrujado y Otras Historias", "Sm Ediciones", 2014, "Sergio Gomez"));
+        listaLibros.add(new libro(3, "9781484256251", "Spring Boot Persistence Best Practices", "Apress", 2020, "Anghel Leonard"));
+        listaLibros.add(new libro(4, "9789566075752", "Harry Potter y la piedra filosofal", "Salamandra", 2024, "J. K. Rowling"));
+        listaLibros.add(new libro(5, "9780439139601", "Harry Potter y el prisionero de Azkaban", "Scholastic", 1999, "J. K. Rowling"));
+        listaLibros.add(new libro(6, "9780439136365", "Harry Potter y el cáliz de fuego", "Scholastic", 2000, "J. K. Rowling"));
+        listaLibros.add(new libro(7, "9780321127426", "Effective Java", "AddisonWesley", 2008, "Joshua Bloch"));
+        listaLibros.add(new libro(8, "9780134685991", "Clean Architecture", "Prentice Hall", 2017, "Robert C. Martin"));
+        listaLibros.add(new libro(9, "9780201633610", "Design Patterns", "AddisonWesley", 1994, "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides"));
+        listaLibros.add(new libro(10, "9780132350884", "Clean Code", "Prentice Hall",2008, "Robert C. Martin"));
+
+    }
+
     //Metodo que retorna todos los libros
     public List<libro> obtenerLibros(){
 
